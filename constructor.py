2d71b3a89377d10289e558a9e4b9484118eee618
Harsh4252 <149177238+Harsh4252@users.noreply.github.com>
2024-08-03 18:11:44 +0530

    Add files via upload

---
+++ constructor.py
@@ -0,0 +1,8 @@
+#constructor __init __ called
+class person:
+    def __init__(self,name ,age):
+        self.name=name
+        self.age=age
+p1=person("john",36)
+print(p1.name)
+print(p1.age)-8**
